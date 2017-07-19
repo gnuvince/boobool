0dee6eae62de68c7f85f8d763a993edef9a3d3a7
Vincent Foley <vfoley@gmail.com>
2017-07-19 11:05:36 -0400

    Add transformation to sort lists
    To help improve the performance of the evaluation phase, lists are
    sorted in non-decreasing order to allow binary search.

--- src/transformers/mod.rs
+++ src/transformers/mod.rs
@@ -2,8 +2,10 @@ use ast::TypedExpr;
 use types::Symtable;
 
 mod ors;
+mod sort_lists;
 
 pub fn transform(mut expr: TypedExpr, st: &Symtable) -> TypedExpr {
     expr = ors::transform_ors(expr, st);
+    expr = sort_lists::sort_lists(expr);
     return expr;
 }
